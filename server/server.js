428473372a49e3d08c1afea16e491d05033214ec
BhusanDotel <bhusandotel1@gmail.com>
2024-04-07 12:52:16 +0545

    Added response for test api

--- server/server.js
+++ server/server.js
@@ -14,6 +14,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api", weatherDataroute);
 
+app.get("/test-webhook", (req, res) => {
+  // Parse the query params
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  // Check if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Check the mode and token sent is correct
+    if (mode === "subscribe" && token === "1234567890") {
+      // Respond with the challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Respond with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+});
+
 app.post("/test-webhook", (req, res) => {
   if (req.body) {
     console.log("Responses", req.body);
